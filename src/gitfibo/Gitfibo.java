e6c0dd178cc5be493ff0ee52d2fc1fa35a96d447
Tany Villalba <tanyvillalba@gmail.com>
2014-11-11 12:34:29 -0800

    Primer commit

---
+++ src/gitfibo/Gitfibo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitfibo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Gitfibo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        fibo fib=new fibo();
+        System.out.println(fib.calcular(0));
+    }
+}
---
+++ src/gitfibo/fibo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitfibo;
+
+/**
+ *
+ * @author usuario
+ */
+public class fibo {
+    public int calcular(int numero)
+    {
+        if (numero <2)
+        {
+            return numero;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
